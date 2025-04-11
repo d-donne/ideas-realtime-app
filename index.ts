14554e37f4c20c6a0b06837f067b23eb75ca3218
d-donne <doublyd@gmail.com>
2025-04-11 21:57:10 +0000

    initialize project

---
+++ index.ts
@@ -0,0 +1,7 @@
+console.log("Hello via Bun!");
+
+import feathers from "@feathersjs/feathers";
+import express from "@feathersjs/express";
+import socketio from "@feathersjs/socketio";
+import moment from "moment";
+
