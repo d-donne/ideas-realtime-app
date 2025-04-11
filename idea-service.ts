4f783bdd2ec8dea464181c9ad4f4cf9d5cc0544d
d-donne <doublyd@gmail.com>
2025-04-11 22:51:03 +0000

    Add initial implementation of Realtime Ideas app with service and HTML form

---
+++ idea-service.ts
@@ -0,0 +1,27 @@
+import moment from "moment";
+
+export class IdeaService {
+    ideas: any[];
+    constructor() {
+        this.ideas = [];
+    }
+
+    async find() {
+        return this.ideas;
+    }
+
+    async create(data: any) {
+        const newIdea = {
+            id: this.ideas.length + 1,
+            text: data.text,
+            tech: data.tech,
+            viewer: data.viewer,
+            time: moment().format("YYYY-MM-DD HH:mm:ss"),
+            
+        }
+
+        this.ideas.push(newIdea);
+        return newIdea;
+
+    }
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,46 @@
-console.log("Hello via Bun!");
-
-import feathers from "@feathersjs/feathers";
-import express from "@feathersjs/express";
+import { feathers, type RealTimeConnection } from "@feathersjs/feathers";
+import express, { json, rest } from "@feathersjs/express";
 import socketio from "@feathersjs/socketio";
-import moment from "moment";
+import { channels } from "@feathersjs/transport-commons";
+import { IdeaService } from "./idea-service";
 
+const PORT = 3030;
+
+const app = express(feathers())
+
+// parse JSON
+app.use(json())
+
+// enable Socket.io support
+app.configure(socketio())
+
+// enable REST services
+app.configure(rest())
+
+// register services
+const ideaService = new IdeaService()
+app.use('/ideas', ideaService)
+
+// configure channels
+app.configure(channels())
+
+// connection to stream channel
+app.on('connection', (connection: RealTimeConnection) => {
+    // On a new real-time connection, add it to the anonymous channel
+    app.channel('stream').join(connection)
+  })
+
+app.publish(data => app.channel('stream'))
+
+
+app.listen(PORT).then((server) => {
+  server.on('listening', () => {
+    console.log(`Realtime server listening on http://localhost:${PORT}`)
+  })
+})
+
+app.service('ideas').create({
+    text: 'Build a realtime app',
+    tech: 'NodeJS',
+    viewer: 'Jane Stephenson',
+})
